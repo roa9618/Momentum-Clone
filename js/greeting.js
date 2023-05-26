257715edc5c7e24dca642d220e7ca66bd33d935c
roa9618 <ykjrco@naver.com>
2023-05-26 09:53:11 +0000

    Create greeting function
    create login-form and save username to use local storage

---
+++ js/greeting.js
@@ -0,0 +1,29 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreeting(userName);
+}
+
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+  paintGreeting(savedUsername);
+}
